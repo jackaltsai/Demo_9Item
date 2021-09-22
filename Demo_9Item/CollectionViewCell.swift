e80b76f6fe907fab5350d620168fe696accb4cad
jackaltsai <hata.s520@gmail.com>
2021-09-22 14:58:13 +0800

    畫面不能捲動的九宮格

---
+++ Demo_9Item/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Demo_9Item
+//
+//  Created by 蔡忠翊 on 2021/9/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- Demo_9Item/ViewController.swift
+++ Demo_9Item/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.collectionViewLayout = generateLayout()
+        collectionView.isScrollEnabled = false
     }
-
-
+    
+    /* 畫面不能捲動的九宮格 */
+    // 除了設定 layout，記得也要將 collection view 的 isScrollEnabled 設為 false
+    
+    func generateLayout() -> UICollectionViewLayout {
+        let space: Double = 3
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: space, leading: space, bottom: space, trailing: space)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: space, leading: space, bottom: space, trailing: space)
+        return UICollectionViewCompositionalLayout(section: section)
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CollectionViewCell.self)", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = UIImage(named: "Image\(indexPath.item + 1)")
+        return cell
+    }
+    
+    
+}
